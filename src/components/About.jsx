70063f9338ea14bf76c342c7b848472124ce07a2
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-06-12 11:48:46 +0530

    code checking

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
     <div className="bg-aboutBackground bg-cover bg-center  bg-no-repeat">
       <div className="container max-w-[1164px] pt-10 md:pt-14 lg:pt-20 md:pb-10 xl:pb-32 mx-auto">
         <div className="flex flex-col-reverse md:flex-row  lg:pt-5 justify-center items-center flex-wrap">
-          <div className="w-full md:w-5/12 px-6 md:px-3  ">
+          <div className="w-full md:w-5/12 px-3 pt-6 md:pt-0 ">
             <img
               className="w-full"
               src={fabElectrolysis}
               alt="electrolysis_image"
             />
-            <p className="text-sm md:text-base font-Poppins px-4 md:px-0 text-center md:text-start text-black leading-xl opacity-80 md:max-w-[461px] font-normal pt-4 md:pt-11 mb-6">
+            <p className="text-sm md:text-base font-Poppins text-center md:text-start text-black leading-xl opacity-80 md:max-w-[461px] font-normal pt-4 md:pt-11 mb-6">
               We understand that each client is unique, which is why we tailor
               our treatments to your specific needs and concerns. Whether you're
               seeking laser hair removal, electrolysis for permanent hair
@@ -31,20 +31,20 @@ const About = () => {
               commonHeading="About Fabelectrolysis"
               className="mb-4 text-center !leading-md md:text-start"
             />
-
-            <p className=" text-base font-Poppins px-4 md:px-0 text-center mx-auto md:text-start text-black leading-xl opacity-80 max-w-[665px] font-normal  mb-10">
-              At FabElectrolysis, we're more than just a skincare clinic – we're
-              a welcoming environment where every individual can feel
-              comfortable and valued. With a commitment to inclusivity and
-              personalized care, we strive to create a supportive atmosphere
-              where you can truly be yourself.
-            </p>
-
-            <img
-              className="w-full px-4 md:px-0"
-              src={electrolysisSetup}
-              alt="electrolysis_image"
-            />
+            <div className="flex-col-reverse flex md:flex-col  ">
+              <p className=" text-base font-Poppins px-4 md:px-0 text-center mx-auto md:text-start text-black leading-xl opacity-80 max-w-[665px] font-normal mb-2 md:mb-10">
+                At FabElectrolysis, we're more than just a skincare clinic –
+                we're a welcoming environment where every individual can feel
+                comfortable and valued. With a commitment to inclusivity and
+                personalized care, we strive to create a supportive atmosphere
+                where you can truly be yourself.
+              </p>
+              <img
+                className="w-full max-sm:min-h-[240px] max-sm:max-w-[400px] px-1 pb-2 md:pb-0 md:px-0"
+                src={electrolysisSetup}
+                alt="electrolysis_image"
+              />{" "}
+            </div>
           </div>
         </div>
       </div>
--- src/components/Technicians.jsx
+++ src/components/Technicians.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import { TechniqueData } from "../common/Helper";
 import Heading from "../common/Heading";
-import { Facebook, Insta, Twiter } from "../common/icon";
 
 const Technicians = () => {
   return (
     <div className="bg-aboutBackground bg-cover bg-center bg-no-repeat min-h-[1350px] xs:min-h-[1600px] sm:min-h-[800px] md:min-h-[900px] lg:min-h-[1000px]">
-      <div className="container max-w-[1164px] pt-10 md:pt-14 lg:pt-20 mx-auto">
+      <div className="container max-w-[1164px] pt-[70px] md:pt-14 lg:pt-20 mx-auto">
         <div className="flex justify-center flex-col items-center">
           <Heading commonHeading="Meet Our Technicians" className="mb-4" />
-          <p className="text-center text-sm sm:text-base font-Poppins text-black opacity-80 max-w-[777px] mb-2 px-6 md:px-0 md:mb-[70px]">
+          <p className="text-center text-sm sm:text-base font-Poppins text-black opacity-80 max-w-[777px]  px-6 md:px-0 md:mb-[70px]">
             Expert technicians specializing in electrolysis and laser hair
             removal, utilizing Candela technology for top-notch care and optimal
             results.
           </p>
         </div>
-        <div className="flex flex-col sm:flex-row lg:gap-5 xl:gap-[31px] pt-10 md:pt-0 justify-center">
+        <div className="flex flex-col sm:flex-row lg:gap-5 xl:gap-[31px] pt-5 md:pt-0 justify-center">
           {TechniqueData.map((technique, index) => (
             <div
               key={index}
-              className={`w-full px-6 sm:px-3 md:px-3 mb-6 md:mb-0 ${
+              className={`w-full px-3 md:px-3 mb-6 md:mb-0 ${
                 index === 1 ? "" : ""
               }`}
             >
-              <div className="border-b-[1.82px] border-[1.82px] border-l-[1.82px] border-r-[1.82px] border-pinkPrimary h-[520px] xsm2:h-[590px] xs:h-[650px] xxs:h-[700px] sm:h-[550px] md:h-[580px] xlg:h-[620px] lg:h-[648px]">
+              <div className="border-b-[1.82px] border-[1.82px] border-l-[1.82px] border-r-[1.82px] border-pinkPrimary h-[540px] xsm2:h-[590px] xs:h-[650px] xxs:h-[700px] sm:h-[550px] md:h-[580px] xlg:h-[620px] lg:h-[648px]">
                 <div className="w-full bg-white pt-[30px] border-b-[1.82px] border-pinkPrimary h-[336px] px-6 lg:px-11 duration-300">
                   <p className="font-normal text-xl2 leading-xl text-center font-Playfair text-black mb-2">
                     {technique.title}
